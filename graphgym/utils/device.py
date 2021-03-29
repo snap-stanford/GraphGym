1b0975bdba79c91a5aed633027cd942ccc6e74f8
Guangxuan Xiao <40906949+Guangxuan-Xiao@users.noreply.github.com>
2021-03-30 07:49:47 +0800

    add get current gpu usage function

--- graphgym/utils/device.py
+++ graphgym/utils/device.py
@@ -4,7 +4,7 @@ import numpy as np
 import pdb
 from graphgym.config import cfg
 import logging
-
+import os
 
 def get_gpu_memory_map():
     '''Get the current gpu usage.'''
@@ -15,6 +15,21 @@ def get_gpu_memory_map():
         ], encoding='utf-8')
     gpu_memory = np.array([int(x) for x in result.strip().split('\n')])
     return gpu_memory
+
+
+def get_current_gpu_usage():
+    result = subprocess.check_output(
+        [
+            'nvidia-smi', '--query-compute-apps=pid,used_memory',
+            '--format=csv,nounits,noheader'
+        ], encoding='utf-8')
+    current_pid = os.getpid()
+    used_memory = 0
+    for line in result.strip().split('\n'):
+        line = line.split(', ')
+        if current_pid == int(line[0]):
+            used_memory += int(line[1])
+    return used_memory
 
 
 def auto_select_device(memory_max=8000, memory_bias=200, strategy='random'):
