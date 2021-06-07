89c5197fdcd4155c89bff2d132af3658413e6817
Tianyu Du <tianyudu@stanford.edu>
2021-06-07 12:58:58 -0700

    remove comments

--- graphgym/utils/stats.py
+++ graphgym/utils/stats.py
@@ -12,15 +12,3 @@ def node_degree(edge_index, n=None, mode='in'):
     degree = torch.zeros(n)
     ones = torch.ones(index.shape[0])
     return degree.scatter_add_(0, index, ones)
-
-
-
-
-
-
-
-# edge_index = torch.tensor([[1, 2, 3, 4], [2, 3, 4, 5]])
-
-# print(compute_degree(edge_index, mode='in'))
-# print(compute_degree(edge_index, mode='out'))
-# print(compute_degree(edge_index, mode='both'))
