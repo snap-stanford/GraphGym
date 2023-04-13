a9594b0de09c672b31ee4ecb5c3f45b0810b08af
luciancahil <royhe62@yahoo.ca|>
2023-04-12 21:38:48 -0700

    fixed proces

--- run/CytokinesDataSet.py
+++ run/CytokinesDataSet.py
@@ -49,6 +49,58 @@ class CytokinesDataSet(Dataset):
     def process(self):
         name = self.graphName[:-10]
         self.new_dir = "datasets\\" + name + "\\processed\\"# "datasets\\" + name + "\\processed"   # new processed dir
+        train_data = Data()
+        train_slices = dict()
+        test_data = Data()
+        test_slices = dict()
+        train_tuple = (train_data, train_slices)
+        test_tuple = (test_data, test_slices)
+
+        num_patients = len(self.patients)
+        num_training = int(0.8*num_patients) # when index <= num_training, the patient goes into training data. Else, testing
+        num_testing = num_patients - num_training
+        num_nodes = len(self._get_node_features(self.patients[0]))
+        num_edges = len(self.adjacency)
+        
+        
+        # prepare the slices
+        train_y_slice = []
+        train_x_slice = []
+        train_edge_index_slice = []
+
+        test_y_slice = []
+        test_x_slice = []
+        test_edge_index_slice = []
+
+        for i in range(num_training + 1):
+            train_y_slice.append(i)
+            train_x_slice.append(num_nodes*i)
+            train_edge_index_slice.append(num_edges * i)
+
+        for i in range(num_testing + 1):
+            test_y_slice.append(i)
+            test_x_slice.append(num_nodes*i)
+            test_edge_index_slice.append(num_edges * i)
+        
+        train_slices['y'] = torch.tensor(train_y_slice)
+        train_slices['x'] = torch.tensor(train_x_slice)
+        train_slices['edge_index'] = torch.tensor(train_edge_index_slice)
+
+        test_slices['y'] = torch.tensor(test_y_slice)
+        test_slices['x'] = torch.tensor(test_x_slice)
+        test_slices['edge_index'] = torch.tensor(test_edge_index_slice)
+        
+
+        node_vector_len = len(self._get_node_features(self.patients[0]).numpy()[0]) # the length of a node vector
+        # preparing data
+        train_x_tensor = torch.empty((0, node_vector_len), dtype=torch.float32)
+        train_edge_index_tensor = torch.empty((2, 0), dtype=torch.float32)
+        train_y_list = [] # It's just easier to make a list than figure out how to do 1d concats
+
+        test_x_tensor = torch.empty((0, node_vector_len), dtype=torch.float32)
+        test_edge_index_tensor = torch.empty((2, 0), dtype=torch.float32)
+        test_y_list = [] # It's just easier to make a list than figure out how to do 1d concats
+
 
         # runs once for each patient
         index = 0
@@ -60,25 +112,46 @@ class CytokinesDataSet(Dataset):
             # Get adjacency info of a single patient
             edge_index = self._get_adjacency_info()
             # Get labels info of a single patient
-            label = self._get_labels(patient[CLASS_NAME]) # todo: read label from patient data
+            label = self._get_labels(patient[CLASS_NAME])
 
-
-            # Create data object
-            data = Data(x=node_feats, 
-                        edge_index=edge_index,
-                        edge_attr=edge_feats,
-                        y=label,
-                        ) 
-            if self.test:
-                torch.save(data, 
-                    os.path.join(self.new_dir, 
-                                 f'{name}_data_test_{index}.pt'))
+            if index <= num_training:
+                train_x_tensor = torch.cat((train_x_tensor,node_feats), 0)
+                train_edge_index_tensor = torch.cat((train_edge_index_tensor,edge_index), 1)
+                train_y_list.append(int(patient[CLASS_NAME]))
             else:
-                torch.save(data, 
-                    os.path.join(self.new_dir, 
-                                 f'{name}_data_{index}.pt'))
-            
+                test_x_tensor = torch.cat((test_x_tensor,node_feats), 0)
+                test_edge_index_tensor = torch.cat((test_edge_index_tensor,edge_index), 1)
+                test_y_list.append(int(patient[CLASS_NAME]))
+
             index += 1
+
+        # turn y_lists into tensors
+        train_y_tensor = torch.tensor(train_y_list)
+        test_y_tensor = torch.tensor(test_y_list)
+
+        train_data.x = train_x_tensor
+        train_data.y = train_y_tensor
+        train_data.edge_index = train_edge_index_tensor
+
+
+        test_data.x = test_x_tensor
+        test_data.y = test_edge_index_tensor
+        test_data.edge_index = test_edge_index_tensor
+
+        torch.save(train_tuple, os.path.join(self.new_dir, 'train_data.pt'))
+        torch.save(test_tuple, os.path.join(self.new_dir, 'test_data.pt'))
+
+
+        # last step, just save everything in the right .pt files
+        """
+                return ['train_data.pt', 'test_data.pt']
+
+                        torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'{name}_data_{index}.pt'))
+        """
+
+        
 
     def _get_node_features(self, patient):
         """ 
